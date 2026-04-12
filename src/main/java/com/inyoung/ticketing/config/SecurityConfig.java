a40836546c95be23564d080cfb5b2e4d68a43eab
김인영 <kiy3035@naver.com>
2026-04-12 16:21:09 +0900

    테스트

--- src/main/java/com/inyoung/ticketing/config/SecurityConfig.java
+++ src/main/java/com/inyoung/ticketing/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.inyoung.ticketing.auth.jwt.JwtAuthenticationFilter;
 import com.inyoung.ticketing.auth.jwt.JwtAuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -44,6 +46,11 @@ public class SecurityConfig {
 			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
 	}
 
 	@Bean
